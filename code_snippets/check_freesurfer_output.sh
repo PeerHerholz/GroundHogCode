4b5034f13480699e72923f1620feb99854d6b65b
Peer Herholz <herholz.peer@gmail.com>
2018-01-10 15:16:44 +0100

    Update check_freesurfer_output.sh
    - included more information about function
    - included path check

--- code_snippets/check_freesurfer_output.sh
+++ code_snippets/check_freesurfer_output.sh
@@ -1,12 +1,36 @@
-# This bash script loads data processed with freesurfer stored under data_path into freeview to check if everything went well
-# data_path should be your subject directory along with the specific subject
+# check_freesurfer_output.sh
 
+# This bash script loads data processed with freesurfer stored under data_path into freeview to do an initial check of some recon-all outputs 
+# It currently takes one input variable, which is "data_path". This should be your subject directory along with the specific subject. 
+
+# dependencies: - freesurfer
+
+# version 1, 01/2018 - Malte Gueth (maltegueth@gmail.com)
+#                    - Peer Herholz (herholz.peer@gmail.com)
+
+# usage example
+
+# bash check_freesurfer_output.sh /path/to/your/dataset/derivatives/freesurfer/sub-01/
+
+# function input 
 data_path=$1
-echo data_path is $data_path
 
-freeview -v $data_path/mri/brainmask.mgz \
-         -v $data_path/mri/aseg.mgz:colormap=lut:opacity=0.2 \
-         -f $data_path/surf/lh.white:edgecolor=yellow \
-         -f $data_path/surf/rh.white:edgecolor=yellow \
-         -f $data_path/surf/lh.pial:annot=aparc:edgecolor=red \
-         -f $data_path/surf/rh.pial:annot=aparc:edgecolor=red
+# display data_path
+echo "data_path is $data_path"
+
+# check if data_path exists empty
+if [ ! -d "$data_path/mri/" ]; then
+  echo "the folder $data_path/mri doesn't exist, check again and make sure that your path is correct"
+fi
+
+if [ ! -d "$data_path/surf/" ]; then
+  echo "the folder $data_path/surf doesn't exist, check again and make sure that your path is correct"
+fi
+
+# start freeview and load respective outputs
+freeview -v $data_path/mri/brainmask.mgz \ # load brainmask
+         -v $data_path/mri/aseg.mgz:colormap=lut:opacity=0.2 \ # load aseg segmentation, overlay on brainmask
+         -f $data_path/surf/lh.white:edgecolor=yellow \ # load lh white surface, overlay in yellow 
+         -f $data_path/surf/rh.white:edgecolor=yellow \ # load rh white surface, overlay in yellow
+         -f $data_path/surf/lh.pial:annot=aparc:edgecolor=red \ # load lh pial surface, annotation=aparc, overlay in red
+         -f $data_path/surf/rh.pial:annot=aparc:edgecolor=red # load rh pial surface, annotation=aparc, overlay in red
